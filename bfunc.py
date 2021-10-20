f5dfdc6eb208bd461b424d7bf87c7f23e3fda1f7
Javier García Vázquez <53878105+JavGVastro@users.noreply.github.com>
2021-10-20 18:37:57 -0500

    Updated version of bfunc
    +bfunc04s
    This new form considers seeing, noise and box size in the standard functional.

--- bfunc.py
+++ bfunc.py
@@ -1,66 +1,48 @@
+"""
+New improved structure function models 
+
+Will Henney 2021-10-16: Hopefully, these are the final versions
+
+bfunc00s is the basic model for B(r) based on an exponential autocorrelation function
+bfunc03s includes the effect of seeing with rms width s0 plus noise
+bfunc04s is the same with additional effect of finite box size
+
+See new-model-strucfuncs.{py,ipynb} for more details
+"""
 import numpy as np
 
 
-def bfunc00(r, r0, sig2, m):
-    "Simple 3-parameter structure function"
-    C = 1.0 / (1.0 + (r / r0)**m)
-    return 2.0 * sig2 * (1.0 - C)
-
-def seeing(r, s0):
-    return (np.tanh((r / (2.0 * s0))**2))**2
-
-def bfunc01(r, r0, sig2, m, s0):
-    "Structure function with added seeing (scale `s0`)"
-    return seeing(r, s0) * bfunc00(r, r0, sig2, m)
-
-def bfunc02(r, r0, sig2, m, s0, noise):
-    "Structure function with added seeing (scale `s0`) and noise"
-    return seeing(r, s0) * bfunc00(r, r0, sig2, m) + noise
-
-def seeing_empirical(r, s0, r0):
-    return bfac(s0 / r0) * ratio_empirical(r, s0, a=0.75)
-
-def bfunc03(r, r0, sig2, m, s0, noise):
-    "Structure function with better seeing (scale `s0`) and noise"
-    return seeing_empirical(r, s0, r0) * bfunc00(r, r0, sig2, m) + noise
-
 def bfunc00s(r, r0, sig2, m):
     "Simple 3-parameter structure function"
-    C = np.exp(-np.log(2) * (r / r0)**m)
+    C = np.exp(-np.log(2) * (r / r0) ** m)
     return 2.0 * sig2 * (1.0 - C)
+
+
+def seeing_large_scale(s0, r0):
+    return np.exp(-s0 / r0)
+
+
+def seeing_empirical(r, s0, r0, a=1.5):
+    """
+    Simplified version of empirical fit to B(r) reduction from seeing
+    """
+    return seeing_large_scale(s0, r0) / (1 + (2 * s0 / r) ** a)
+
 
 def bfunc03s(r, r0, sig2, m, s0, noise):
     "Structure function with better seeing (scale `s0`) and noise"
     return seeing_empirical(r, s0, r0) * bfunc00s(r, r0, sig2, m) + noise
 
-def ratio_empirical(rad, s0, a=1.0):
-    """
-    Simple tanh law in semi-log space to fit the seeing
 
-    Reduction in B(r) is always 0.5 when r = 2 * s0
-    Parameter `a` controls the slope of the transition.
-    """
-    x = np.log(rad / (2 * s0))
-    y = np.tanh(a * x)
-    return 0.5 * (1.0 + y)
+def finite_box_effect(r0, L, scale=3.6):
+    return 1 - np.exp(-L / (scale * r0))
 
-def rtheo(rad, s0, s00=2, **kwds):
-    """
-    Theoretical ratio of B(r) structure function
 
-    For an additional seeing FWHM of s0, assuming that
-    the original seeing was s00 (widths assumed to add in quadrature)
-    """
-    s1 = np.hypot(s0, s00)
+def bfunc04s(r, r0, sig2, m, s0, noise, box_size):
+    "Structure function with better seeing (scale `s0`) and noise, plus finite box effect"
+    boxeff = finite_box_effect(r0, box_size)
     return (
-        ratio_empirical(rad, s1, **kwds)
-        / ratio_empirical(rad, s00, **kwds)
+        # Note that the seeing is unaffected by boxeff
+        seeing_empirical(r, s0, r0) * bfunc00s(r, boxeff * r0, boxeff * sig2, m)
+        + noise
     )
-
-def bfac(x):
-    """
-    Across-the board reduction in B(r) for x = s0 / r0
-
-    Where s0 is RMS seeing width and r0 is correlation length
-    """
-    return 1 / (1 + 4*x**2)
