337d59dce6036b689d3a00edfd0546628e1892d8
JavGVastro <jav.astro.ipn@hotmail.com>
2022-06-04 13:28:37 -0500

    Updated methods

---
+++ structure-functions/sf-Hanel-EON-O.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import time
+start_time=time.time()
+
+
+from pathlib import Path
+
+from matplotlib import pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+from scipy import interpolate
+import json
+
+import strucfunc
+from strucfunc import sosflog
+import bfunc
+
+
+# Input path: velocity fields folder
+
+text_file_0 = open("path-data.txt", "r")
+path_data = text_file_0.read()
+
+
+datapath_data = Path(path_data).expanduser()
+
+
+# #Output path: results folder location
+
+#text_file_1 = open("path-results.txt", "r")
+#path_res = text_file_1.read()
+
+
+#datapath_res= Path(path_res).expanduser()
+
+
+# File names
+
+name = 'Hanel-EON-O'
+
+
+data = json.load(open(str(datapath_data) + '/' + name + "-m.json"))
+#sb = np.array(data["sb"])
+vv = np.array(data["vv"])
+#ss = np.array(data["ss"])
+
+
+vv[vv== 'nan'] = vv.mean()
+
+
+## Replace spurious values in the arrays
+#m = ~np.isfinite(sb*vv) | (sb < 0.0)
+
+#sb[m] = 0.0
+#vv[m] = np.nanmean(vv)
+#ss[m] = 0.0
+#sb /= sb.max()
+#good = (~m) & (sb > 0.001)
+
+
+m = ~np.isfinite(vv) 
+vv[m] = np.nanmean(vv)
+good = (~m) 
+
+
+box_size = np.sqrt(vv.shape[0] * vv.shape[1]) * data['pix'] * data['pc']
+
+
+rslt = strucfunc.strucfunc_numba_parallel(vv, dlogr=0.05)
+
+
+rslt
+
+
+
+
+
+rslt["Unweighted mean velocity"] = np.mean(vv[good])
+rslt["Unweighted sigma^2"] = np.var(vv[good])
+#v0w = rslt["Weighted mean velocity"] = np.average(vv, weights=sb)
+#rslt["Weighted sigma^2"] = np.average((vv - v0w)**2, weights=sb)
+
+
+b2 = rslt['Unweighted B(r)']
+#b2w = rslt['Weighted B(r)']
+e_b2 = b2  *.1
+#e_b2w = b2w  *.1
+
+s = 10**rslt['log10 r'] * data['pix'] * data['pc']
+e_s = s  *.1
+sig2 = rslt['Unweighted sigma^2']
+#sig2w = rslt['Weighted sigma^2']
+
+sig = sig2**0.5
+
+
+r0 = np.interp(sig2[1:10], b2[1:10], s)
+r0
+
+
+r1 = np.interp(sig[1:8], b2[1:8], s,period = 360)
+r1
+
+
+r2 = np.interp(2*sig2, b2, s, period = 360)
+r2
+
+
+m = 0.5
+s0 = data['s0']
+noise = 2
+
+fig, ax = plt.subplots(figsize=(8, 6))
+rgrid = np.logspace(-2, 1)
+ax.errorbar(s, b2, yerr=e_b2, xerr=e_s, fmt='o', alpha=0.4,color="blue")
+#ax.errorbar(s, b2w, yerr=e_b2w, xerr=e_s, fmt='x', alpha=0.4,color="green")
+
+ax.axhline(sig2, ls='-')
+ax.axvline(r0, ls='-')
+
+ax.axhline(2*sig2, ls=':')
+ax.axvline(r2, ls=':')
+
+ax.axhline(sig, ls=':')
+ax.axvline(r1, ls=':')
+
+ax.plot(rgrid, bfunc.bfunc00s(rgrid, r0, sig2, m), color="0.8")
+ax.plot(rgrid, bfunc.bfunc03s(rgrid, r0, sig2, m, s0, noise), color="red")
+ax.plot(rgrid, bfunc.bfunc04s(rgrid, r0, sig2, m, s0, noise, box_size), color="black")
+
+
+ax.set(xscale='log', yscale='log', 
+       xlabel='separation, pc',
+       ylabel=r'$b^2,\ \mathrm{km^2\ s^{-2}}$'
+      )
+None
+
+sig2, r0, m
+
+
+results = {
+    'sig2' : sig2,
+        'r0' : r0,
+        'r1' : r1,
+        'r2' : r2,
+          } 
+
+
+data_export = {   
+        's' : s,
+        'b2' : b2,
+        'pc' : data['pc'],
+        'pix' : data['pix'],
+        'box_size': box_size,
+         'results':results,
+         'SF' : rslt,
+         'VF' : data,
+}
+
+
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(MyEncoder, self).default(obj)
+
+
+jsonfilename ="sf-" + name +".json"
+with open(jsonfilename, "w") as f:
+    json.dump(data_export, fp=f, indent=3, cls=MyEncoder)
+
+
+print("--- %s seconds ---" % (time.time()-start_time))
+
+
+get_ipython().system('jupyter nbconvert --to script --no-prompt sf-Hanel-EON-O.ipynb')
+
---
+++ structure-functions/sf-MUSE-Dor-N.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import time
+start_time=time.time()
+
+
+from pathlib import Path
+
+from matplotlib import pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+from scipy import interpolate
+import json
+
+import strucfunc
+from strucfunc import sosflog
+import bfunc
+
+
+# Input path: velocity fields folder
+
+text_file_0 = open("path-data.txt", "r")
+path_data = text_file_0.read()
+
+
+datapath_data = Path(path_data).expanduser()
+
+
+# File names
+
+name = 'MUSE-Dor-N'
+
+
+data = json.load(open(str(datapath_data) + '/' + name + "-m.json"))
+sb = np.array(data["sb"])
+vv = np.array(data["vv"])
+ss = np.array(data["ss"])
+
+
+## Replace spurious values in the arrays
+m = ~np.isfinite(sb*vv*ss) | (sb < 0.0)
+m = m | (sb > 6e4) 
+sb[m] = 0.0
+vv[m] = np.nanmean(vv)
+ss[m] = 0.0
+sb /= sb.max()
+
+good = (~m) & (sb > 0.001)
+
+
+box_size = np.sqrt(sb.shape[0] * sb.shape[1]) * data['pix'] * data['pc']
+
+
+rslt = strucfunc.strucfunc_numba_parallel(vv, wmap=sb, dlogr=0.05)
+
+
+rslt["Unweighted mean velocity"] = np.mean(vv[good])
+rslt["Unweighted sigma^2"] = np.var(vv[good])
+v0w = rslt["Weighted mean velocity"] = np.average(vv, weights=sb)
+rslt["Weighted sigma^2"] = np.average((vv - v0w)**2, weights=sb)
+
+
+b2 = rslt['Unweighted B(r)']
+b2w = rslt['Weighted B(r)']
+e_b2 = b2  *.1
+e_b2w = b2w  *.1
+
+s = 10**rslt['log10 r'] * data['pix'] * data['pc']
+e_s = s  *.1
+sig2 = rslt['Unweighted sigma^2']
+sig2w = rslt['Weighted sigma^2']
+
+sig = sig2**0.5
+
+
+r0 = np.interp(sig2, b2, s)
+r0
+
+
+# x = s
+# y = b2 - sig2
+# tck=interpolate.splrep(x,y,s=0)
+# grid=np.linspace(x.min(),x.max(),num=len(x))
+# ynew=interpolate.splev(grid,tck,der=0)
+# inter=pd.DataFrame([grid,ynew]).T
+# SFr=interpolate.sproot(tck)
+# r0 = SFr[0]
+
+r1 = np.interp(sig, b2, s)
+r1
+
+
+r2 = np.interp(2*sig2, b2, s)
+r2
+
+
+m = 1.0
+s0 = data['s0']
+noise = 1
+
+
+fig, ax = plt.subplots(figsize=(8, 6))
+rgrid = np.logspace(0, 2.15)
+ax.errorbar(s, b2, yerr=e_b2, xerr=e_s, fmt='o', alpha=0.4,color="blue")
+ax.errorbar(s, b2w, yerr=e_b2w, xerr=e_s, fmt='x', alpha=0.4,color="green")
+
+ax.axhline(sig2, ls='-')
+ax.axvline(r0, ls='-')
+
+ax.axhline(2*sig2, ls=':')
+ax.axvline(r2, ls=':')
+
+ax.axhline(sig, ls=':')
+ax.axvline(r1, ls=':')
+
+ax.plot(rgrid, bfunc.bfunc00s(rgrid, r0, sig2, m), color="0.8")
+ax.plot(rgrid, bfunc.bfunc03s(rgrid, r0, sig2, m, s0, noise), color="red")
+ax.plot(rgrid, bfunc.bfunc04s(rgrid, r0, sig2, m, s0, noise, box_size), color="black")
+
+
+ax.set(xscale='log', yscale='log', 
+       xlabel='separation, pc',
+       ylabel=r'$b^2,\ \mathrm{km^2\ s^{-2}}$'
+      )
+None
+
+sig2, r0, m
+
+
+results = {
+    'sig2' : sig2,
+        'r0' : r0,
+        'r1' : r1,
+        'r2' : r2,
+          } 
+
+
+data_export = {   
+        's' : s,
+        'b2' : b2,
+        'pc' : data['pc'],
+        'pix' : data['pix'],
+        'box_size': box_size,
+         'results':results,
+         'SF' : rslt,
+         'VF' : data,
+}
+
+
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(MyEncoder, self).default(obj)
+
+
+jsonfilename ="sf-" + name +".json"
+with open(jsonfilename, "w") as f:
+    json.dump(data_export, fp=f, indent=3, cls=MyEncoder)
+
+
+print("--- %s seconds ---" % (time.time()-start_time))
+
+
+get_ipython().system('jupyter nbconvert --to script --no-prompt sf-MUSE-Dor-N.ipynb')
+
